232fd7f134e49407adfa2490f11986a3551e2022
Kingsley Okpo <kngsley2018@gmail.com>
2024-12-26 11:19:09 +0100

    creates hash password function

---
+++ app/routes/auth.py
@@ -0,0 +1,3 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+
+auth = APIRouter(tags="Auth")
---
+++ app/utils/auth.py
@@ -0,0 +1,6 @@
+def hash_password():
+    return "password hash"
+
+
+def verify_password():
+    return "verify password"
